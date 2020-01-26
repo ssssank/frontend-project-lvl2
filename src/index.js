4260a879f3492057b965d342a064bad3451fe6c0
Alex Litvinov <ssssank@gmail.com>
2020-01-26 12:37:09 +0300

    Add arguments and option to command

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const pkg = require("../package.json");
 const program = new commander.Command();
 program
   .version(pkg.version)
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig, secondConfig);
+  });
 
 export { program };
