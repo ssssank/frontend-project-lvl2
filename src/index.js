9ca065d16d6d6c161b00caa6ad5be02b5a352c99
Alex Litvinov <ssssank@gmail.com>
2020-01-29 21:34:36 +0300

    Add diff function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import commander from 'commander';
+import fs from 'fs';
+import _ from 'lodash';
 
 const pkg = require('../package.json');
+
+const diff = (obj1, obj2) => {
+  const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
+  const res = [];
+
+  keys.forEach((key) => {
+    if (_.has(obj1, key)) {
+      if (_.has(obj2, key)) {
+        if (obj1[key] === obj2[key]) {
+          res.push(`  ${key}: ${obj1[key]}`);
+        } else {
+          res.push(`- ${key}: ${obj1[key]}`);
+          res.push(`+ ${key}: ${obj2[key]}`);
+        }
+      } else {
+        res.push(`- ${key}: ${obj1[key]}`);
+      }
+    } else {
+      res.push(`+ ${key}: ${obj2[key]}`);
+    }
+  });
+
+  return `{ ${res.join(' ')} }`;
+};
 
 const program = new commander.Command();
 program
@@ -9,7 +35,10 @@ program
   .option('-f, --format <type>', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
-    console.log(firstConfig, secondConfig);
+    const file1 = JSON.parse(fs.readFileSync(firstConfig));
+    const file2 = JSON.parse(fs.readFileSync(secondConfig));
+
+    console.log(diff(file1, file2));
   });
 
 export default program;
