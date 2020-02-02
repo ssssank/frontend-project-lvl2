73e259fa9c0c088db304e456503e77f6de404117
Alex Litvinov <ssssank@gmail.com>
2020-02-02 13:20:45 +0300

    Add fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
 import gendiff from '../src/gendiff';
 
 test('gendiff', () => {
-  expect(gendiff({}, {})).toEqual('{  }');
+  const right = fs.readFileSync(`${__dirname}/../fixtures/right`, 'utf-8');
+  const before = fs.readFileSync(`${__dirname}/../fixtures/before.json`, 'utf-8');
+  const after = fs.readFileSync(`${__dirname}/../fixtures/after.json`, 'utf-8');
+  expect(gendiff({}, {})).toEqual('{\n}');
+  expect(gendiff(JSON.parse(before), JSON.parse(after))).toEqual(right);
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -2,26 +2,26 @@ import _ from 'lodash';
 
 const gendiff = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
-  const res = [];
+  const res = ['\n'];
 
   keys.forEach((key) => {
     if (_.has(obj1, key)) {
       if (_.has(obj2, key)) {
         if (obj1[key] === obj2[key]) {
-          res.push(`  ${key}: ${obj1[key]}`);
+          res.push(`  ${key}: ${obj1[key]}\n`);
         } else {
-          res.push(`- ${key}: ${obj1[key]}`);
-          res.push(`+ ${key}: ${obj2[key]}`);
+          res.push(`- ${key}: ${obj1[key]}\n`);
+          res.push(`+ ${key}: ${obj2[key]}\n`);
         }
       } else {
-        res.push(`- ${key}: ${obj1[key]}`);
+        res.push(`- ${key}: ${obj1[key]}\n`);
       }
     } else {
-      res.push(`+ ${key}: ${obj2[key]}`);
+      res.push(`+ ${key}: ${obj2[key]}\n`);
     }
   });
 
-  return `{ ${res.join(' ')} }`;
+  return `{${res.join(' ')}}`;
 };
 
 export default gendiff;
