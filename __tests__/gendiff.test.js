9499d8c841c69fcf1952aab5e357c716f5ff0a42
Alex Litvinov <ssssank@gmail.com>
2020-02-03 17:08:04 +0300

    Add test for yml file

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,19 @@
 import fs from 'fs';
+import yaml from 'js-yaml';
 import gendiff from '../src/gendiff';
 
-test('gendiff', () => {
+test('json', () => {
   const right = fs.readFileSync(`${__dirname}/../fixtures/right`, 'utf-8');
   const before = fs.readFileSync(`${__dirname}/../fixtures/before.json`, 'utf-8');
   const after = fs.readFileSync(`${__dirname}/../fixtures/after.json`, 'utf-8');
   expect(gendiff({}, {})).toEqual('{\n}');
   expect(gendiff(JSON.parse(before), JSON.parse(after))).toEqual(right);
+});
+
+test('yaml', () => {
+  const right = fs.readFileSync(`${__dirname}/../fixtures/right`, 'utf-8');
+  const before = fs.readFileSync(`${__dirname}/../fixtures/before.yml`, 'utf-8');
+  const after = fs.readFileSync(`${__dirname}/../fixtures/after.yml`, 'utf-8');
+  expect(gendiff({}, {})).toEqual('{\n}');
+  expect(gendiff(yaml.safeLoad(before), yaml.safeLoad(after))).toEqual(right);
 });
