17274a5aff537923f00759720a35282282a3e135
Alex Litvinov <ssssank@gmail.com>
2020-02-04 15:59:50 +0300

    Add test for ini files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 import gendiff from '../src/gendiff';
 
 test('json', () => {
@@ -16,4 +17,12 @@ test('yaml', () => {
   const after = fs.readFileSync(`${__dirname}/../fixtures/after.yml`, 'utf-8');
   expect(gendiff({}, {})).toEqual('{\n}');
   expect(gendiff(yaml.safeLoad(before), yaml.safeLoad(after))).toEqual(right);
+});
+
+test('ini', () => {
+  const right = fs.readFileSync(`${__dirname}/../fixtures/right_ini`, 'utf-8');
+  const before = fs.readFileSync(`${__dirname}/../fixtures/before.ini`, 'utf-8');
+  const after = fs.readFileSync(`${__dirname}/../fixtures/after.ini`, 'utf-8');
+  expect(gendiff({}, {})).toEqual('{\n}');
+  expect(gendiff(ini.parse(before), ini.parse(after))).toEqual(right);
 });
