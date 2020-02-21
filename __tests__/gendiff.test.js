012829cd37f35f4d6ffc27897c377524f01f1871
Alex Litvinov <ssssank@gmail.com>
2020-02-21 11:11:57 +0300

    Continue to use test each

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,27 +5,19 @@ import gendiff from '../src';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(filename, 'utf-8');
 
-test('plain output', () => {
-  const right = readFile(getFixturePath('right_plain'));
-  const before = getFixturePath('before.json');
-  const after = getFixturePath('after.json');
-  expect(gendiff(before, after, 'plain')).toEqual(right);
-});
-
-test('json output', () => {
-  const right = readFile(getFixturePath('right_json'));
-  const before = getFixturePath('before.json');
-  const after = getFixturePath('after.json');
-  expect(gendiff(before, after, 'json')).toEqual(right);
-});
-
 test.each([
-  ['before.json', 'after.json', 'right_text'],
-  ['before.yml', 'after.yml', 'right_text'],
-  ['before.ini', 'after.ini', 'right_text'],
-])('test %s file', (before, after, right) => {
-  const rightFile = readFile(getFixturePath(right));
-  const beforeFile = getFixturePath(before);
-  const afterFile = getFixturePath(after);
-  expect(gendiff(beforeFile, afterFile)).toEqual(rightFile);
+  ['json', 'text'],
+  ['yml', 'text'],
+  ['ini', 'text'],
+  ['json', 'plain'],
+  ['yml', 'plain'],
+  ['ini', 'plain'],
+  ['json', 'json'],
+  ['yml', 'json'],
+  ['ini', 'json'],
+])('test %s file with %s format', (ext, format) => {
+  const rightFile = readFile(getFixturePath(`right_${format}`));
+  const beforeFile = getFixturePath(`before.${ext}`);
+  const afterFile = getFixturePath(`after.${ext}`);
+  expect(gendiff(beforeFile, afterFile, format)).toEqual(rightFile);
 });
