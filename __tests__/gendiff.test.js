72176ddaa02573897caa3d68795fa1640eb4b6c7
Alex Litvinov <ssssank@gmail.com>
2020-02-21 14:35:22 +0300

    Simpify tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,17 +5,14 @@ import gendiff from '../src';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(filename, 'utf-8');
 
-test.each([
-  ['json', 'text'],
-  ['yml', 'text'],
-  ['ini', 'text'],
-  ['json', 'plain'],
-  ['yml', 'plain'],
-  ['ini', 'plain'],
-  ['json', 'json'],
-  ['yml', 'json'],
-  ['ini', 'json'],
-])('test %s file with %s format', (ext, format) => {
+const fileExtentions = ['json', 'yml', 'ini'];
+const outputFormats = ['text', 'plain', 'json'];
+
+const testInput = outputFormats.flatMap((format) => (
+  fileExtentions.map((filetype) => [filetype, format])
+));
+
+test.each(testInput)('test %s files with %s output format', (ext, format) => {
   const rightFile = readFile(getFixturePath(`right_${format}`));
   const beforeFile = getFixturePath(`before.${ext}`);
   const afterFile = getFixturePath(`after.${ext}`);
