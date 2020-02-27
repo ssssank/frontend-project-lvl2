455e5a815c20803e51de3e5e292fe1203cd64aeb
Alex Litvinov <ssssank@gmail.com>
2020-02-27 18:23:08 +0300

    Move file read to higher level

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import fs from 'fs';
+import path from 'path';
 import parse from './parse';
 import buildAST from './buildAST';
 import formatter from './formatters';
 
-const gendiff = (fileBefore, fileAfter, format = 'text') => {
-  const objBefore = parse(fileBefore);
-  const objAfter = parse(fileAfter);
-  const AST = buildAST(objBefore, objAfter);
+const gendiff = (filepathBefore, filepathAfter, format = 'text') => {
+  const contentBefore = fs.readFileSync(filepathBefore, 'utf-8');
+  const contentAfter = fs.readFileSync(filepathAfter, 'utf-8');
+
+  const typeBefore = path.extname(filepathBefore).slice(1);
+  const typeAfter = path.extname(filepathAfter).slice(1);
+
+  const dataBefore = parse(contentBefore, typeBefore);
+  const dataAfter = parse(contentAfter, typeAfter);
+
+  const AST = buildAST(dataBefore, dataAfter);
+
   const render = formatter(format);
+
   return render(AST);
 };
 
--- src/parse.js
+++ src/parse.js
@@ -1,18 +1,12 @@
-import path from 'path';
-import fs from 'fs';
 import ini from 'ini';
 import yaml from 'js-yaml';
 
 const parserMap = {
-  '.json': JSON.parse,
-  '.yml': yaml.safeLoad,
-  '.ini': ini.parse,
+  json: JSON.parse,
+  yml: yaml.safeLoad,
+  ini: ini.parse,
 };
 
-const parse = (file) => {
-  const format = path.extname(file);
-  const data = fs.readFileSync(file, 'utf-8');
-  return parserMap[format](data);
-};
+const parse = (content, type) => parserMap[type](content);
 
 export default parse;
