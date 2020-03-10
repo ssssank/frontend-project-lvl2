7050d238d859cf04d51a1cc556b7a06dcf8aa586
Alex Litvinov <ssssank@gmail.com>
2020-03-10 23:38:07 +0300

    Rewrite buildAST function

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,45 +1,64 @@
 import _ from 'lodash';
 
+const compareValues = (first, second) => {
+  if (_.isObject(first) && _.isObject(second)) {
+    return 'nested';
+  }
+  if (first === second) {
+    return 'not modified';
+  }
+  if (!first) {
+    return 'added';
+  }
+  if (!second) {
+    return 'deleted';
+  }
+  if (first !== second) {
+    return 'modified';
+  }
+  return 'uknown status';
+};
+
 const buildAST = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
-  const AST = keys.map((key) => {
-    if (_.has(obj1, key)) {
-      if (_.has(obj2, key)) {
-        if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-          return {
-            name: key,
-            status: 'nested',
-            children: buildAST(obj1[key], obj2[key]),
-          };
-        }
-        if (obj1[key] === obj2[key]) {
-          return {
-            name: key,
-            value: obj1[key],
-            status: 'not modified',
-          };
-        }
+  return keys.map((key) => {
+    const status = compareValues(obj1[key], obj2[key]);
+    switch (status) {
+      case 'nested':
+        return {
+          name: key,
+          status,
+          children: buildAST(obj1[key], obj2[key]),
+        };
+      case 'modified':
         return {
           name: key,
           value: obj1[key],
           valueNew: obj2[key],
           status: 'modified',
         };
-      }
-      return {
-        name: key,
-        value: obj1[key],
-        status: 'deleted',
-      };
+      case 'not modified':
+        return {
+          name: key,
+          value: obj1[key],
+          status,
+        };
+      case 'deleted':
+        return {
+          name: key,
+          value: obj1[key],
+          status: 'deleted',
+        };
+      case 'added':
+        return {
+          name: key,
+          value: obj2[key],
+          status: 'added',
+        };
+      default:
+        throw new Error(`Unknown status of key '${key}'!`);
     }
-    return {
-      name: key,
-      value: obj2[key],
-      status: 'added',
-    };
   });
-
-  return AST;
 };
 
 export default buildAST;
