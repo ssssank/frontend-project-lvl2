f29eff24ac4195a3741dd931a35a96983be9f766
Alex Litvinov <ssssank@gmail.com>
2020-03-19 17:34:56 +0300

    Delete confition in plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,18 +7,18 @@ const render = (AST) => {
     const res = tree
       .filter((key) => key.status !== 'not modified')
       .map((key) => {
-        const newPath = path.length === 0 ? `${key.name}` : `${path}.${key.name}`;
+        const newPath = [...path, key.name];
         const value = getValue(key.value);
         const modifiedValue = getValue(key.valueNew);
         switch (key.status) {
           case 'nested':
             return iter(key.children, newPath);
           case 'modified':
-            return `Property '${newPath}' was changed from ${value} to ${modifiedValue}`;
+            return `Property '${newPath.join('.')}' was changed from ${value} to ${modifiedValue}`;
           case 'deleted':
-            return `Property '${newPath}' was deleted`;
+            return `Property '${newPath.join('.')}' was deleted`;
           case 'added':
-            return `Property '${newPath}' was added with value: ${value}`;
+            return `Property '${newPath.join('.')}' was added with value: ${value}`;
           default:
             throw new Error(`Unknown status of key '${key.status}'!`);
         }
@@ -27,7 +27,7 @@ const render = (AST) => {
     return res.join('\n');
   };
 
-  return iter(AST, '');
+  return iter(AST, []);
 };
 
 export default render;
