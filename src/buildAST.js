a450f80412a316ace3a364f333fc1fe61399343a
Alex Litvinov <ssssank@gmail.com>
2020-03-19 18:27:27 +0300

    Rewrite buildAST function

--- src/buildAST.js
+++ src/buildAST.js
@@ -5,7 +5,7 @@ const compareValues = (first, second) => {
     return 'nested';
   }
   if (first === second) {
-    return 'not modified';
+    return 'notModified';
   }
   if (!first) {
     return 'added';
@@ -19,45 +19,24 @@ const compareValues = (first, second) => {
   return 'uknown status';
 };
 
+const nodes = {
+  nested: (obj1, obj2, key, fn) => ({ name: key, status: 'nested', children: fn(obj1[key], obj2[key]) }),
+  modified: (obj1, obj2, key) => ({
+    name: key, value: obj1[key], valueNew: obj2[key], status: 'modified',
+  }),
+  notModified: (obj1, obj2, key) => ({ name: key, value: obj1[key], status: 'not modified' }),
+  deleted: (obj1, obj2, key) => ({ name: key, value: obj1[key], status: 'deleted' }),
+  added: (obj1, obj2, key) => ({ name: key, value: obj2[key], status: 'added' }),
+};
+
+const getNode = (status) => nodes[status];
+
 const buildAST = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
   return keys.map((key) => {
     const status = compareValues(obj1[key], obj2[key]);
-    switch (status) {
-      case 'nested':
-        return {
-          name: key,
-          status,
-          children: buildAST(obj1[key], obj2[key]),
-        };
-      case 'modified':
-        return {
-          name: key,
-          value: obj1[key],
-          valueNew: obj2[key],
-          status: 'modified',
-        };
-      case 'not modified':
-        return {
-          name: key,
-          value: obj1[key],
-          status,
-        };
-      case 'deleted':
-        return {
-          name: key,
-          value: obj1[key],
-          status: 'deleted',
-        };
-      case 'added':
-        return {
-          name: key,
-          value: obj2[key],
-          status: 'added',
-        };
-      default:
-        throw new Error(`Unknown status of key '${key}'!`);
-    }
+    const node = getNode(status);
+    return node(obj1, obj2, key, buildAST);
   });
 };
 
