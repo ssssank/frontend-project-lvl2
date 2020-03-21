4395bcc2586ad28a546ed6c9919c3f0a39d0cdbb
Alex Litvinov <ssssank@gmail.com>
2020-03-21 08:26:34 +0300

    Rewrite buildAST function

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,31 +1,47 @@
 import _ from 'lodash';
 
-const compareValues = (first, second, key) => {
-  if (_.isObject(first[key]) && _.isObject(second[key])) {
-    return (obj1, obj2, key1, fn) => ({ name: key1, status: 'nested', children: fn(obj1[key1], obj2[key1]) });
-  }
-  if (first[key] === second[key]) {
-    return (obj1, obj2, key1) => ({ name: key1, value: obj1[key1], status: 'unchanged' });
-  }
-  if (!_.has(first, key)) {
-    return (obj1, obj2, key1) => ({ name: key1, value: obj2[key1], status: 'added' });
-  }
-  if (!_.has(second, key)) {
-    return (obj1, obj2, key1) => ({ name: key1, value: obj1[key1], status: 'deleted' });
-  }
-  if (first[key] !== second[key]) {
-    return (obj1, obj2, key1) => ({
-      name: key1, value: obj1[key1], modifiedValue: obj2[key1], status: 'modified',
-    });
-  }
-  return 'uknown status';
-};
-
-const buildAST = (obj1, obj2) => {
-  const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
+const buildAST = (first, second) => {
+  const keys = _.union(Object.keys(first), Object.keys(second)).sort();
   return keys.map((key) => {
-    const node = compareValues(obj1, obj2, key);
-    return node(obj1, obj2, key, buildAST);
+    if (_.isObject(first[key]) && _.isObject(second[key])) {
+      return {
+        name: key,
+        status: 'nested',
+        children: buildAST(first[key], second[key]),
+      };
+    }
+    if (first[key] === second[key]) {
+      return {
+        name: key,
+        value: first[key],
+        status: 'unchanged',
+      };
+    }
+    if (!_.has(first, key)) {
+      return {
+        name: key,
+        value: second[key],
+        status: 'added',
+      };
+    }
+    if (!_.has(second, key)) {
+      return {
+        name: key,
+        value: first[key],
+        status: 'deleted',
+      };
+    }
+    if (first[key] !== second[key]) {
+      return {
+        name: key,
+        value: first[key],
+        modifiedValue: second[key],
+        status: 'modified',
+      };
+    }
+    return {
+      status: 'unknown',
+    };
   });
 };
 
